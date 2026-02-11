22b882f83dfb614850cf03058f5d77dd006805e9
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-02-11 12:29:02 +0100

    y

--- backup-cosmic-config.sh
+++ backup-cosmic-config.sh
@@ -2,10 +2,10 @@
 set -e
 
 DEST="$HOME/Documents/cosmic-backup"
-mkdir -p "$DEST"
+mkdir -pv "$DEST"
 
-cp -r ~/.config/cosmic ~/.local/share/icons"$DEST"
+cp -rv ~/.config/cosmic ~/.local/share/icons "$DEST"
 dconf dump / > "$DEST/dconf-full-backup.ini"
-cp -r ~/.themes ~/.icons ~/.fonts "$DEST" 2>/dev/null
+#cp -rv ~/.themes ~/.icons ~/.fonts "$DEST" 2>/dev/null
 
 echo "COSMIC backup completed in $DEST"
